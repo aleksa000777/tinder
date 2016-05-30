ea91663adba09e7f6981402c660735058bbbd2bf
Aleksandra Matiyev <aleksa000777@mail.ru>
2016-05-30 12:35:30 -0400

    adds actins and details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function(){
   return [
-    {title: 'Javascript: The Good Parts'},
-    {title: 'Harry Potter'},
-    {title: 'The Dark Tower'},
-    {title: 'OOP Ruby'}
+    {title: 'Javascript: The Good Parts', pages: 101},
+    {title: 'Harry Potter', pages: 32},
+    {title: 'The Dark Tower', pages: 222},
+    {title: 'OOP Ruby', pages: 341}
   ]
 }
